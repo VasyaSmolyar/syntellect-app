a46a7d327830b71275bba855fbbf415596960294
Vasily Smolyar <smolyar4@gmail.com>
2024-07-28 20:41:45 +0300

    feat: buttons base logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,29 @@ function App() {
     <div>
       <TextControl
         textController={textController}
+        leftButtons={[
+          {text: "Очистить текст", callback: () => textController.clearText()}
+        ]}
+        rightButtons={[
+          {text: "Привет мир!", callback: () => textController.setText("Привет мир!")}
+        ]}
       />
       <TextControl
         textController={secondTextController}
+        leftButtons={[
+          {text: "alert c текущим текстом", callback: () => {
+            if (secondTextController.text.length) {
+              alert(secondTextController.text)
+            }
+          }},
+        ]}
+        rightButtons={[
+          {text: "alert, если в инпуте - число", callback: () => {
+            if ( secondTextController.text.trim() !== '' && !isNaN(+secondTextController.text.split(',').join('.'))) { 
+              alert(secondTextController.text)
+            }
+          }}
+        ]}
       />
     </div>
   );
--- src/components/TextControl/TextControl.tsx
+++ src/components/TextControl/TextControl.tsx
@@ -1,15 +1,37 @@
 import { observer } from "mobx-react"
 import TextStore from 'store/textStore'
 
-interface TextControlProps {
-  textController: TextStore;
+interface CallbackButton {
+  text: string;
+  callback: () => void;
 }
 
-const TextControl = observer(({ textController }: TextControlProps) => {
+
+interface TextControlProps {
+  textController: TextStore;
+  leftButtons: CallbackButton[];
+  rightButtons: CallbackButton[];
+}
+
+const TextControl = observer(({ textController, leftButtons, rightButtons }: TextControlProps) => {
 
   return (
     <div>
+      <div>
+        {leftButtons.map((el, i) => (
+          <button key={i} onClick={el.callback}>
+            {el.text}
+          </button>
+        ))}
+      </div>
       <input value={textController.text} onChange={(e) => textController.setText(e.target.value)} /> 
+      <div>
+        {rightButtons.map((el, i) => (
+          <button key={i} onClick={el.callback}>
+            {el.text}
+          </button>
+        ))}
+      </div>
     </div>
   );
 });
